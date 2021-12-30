7249f0836fffc82ec77e295a2136cc338f035f02
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-12-30 21:05:05 +1100

    now()
    - Closes https://github.com/mP1/walkingkooka-tree-expression-function-datetime/issues/6
    - now()

---
+++ src/test/java/walkingkooka/tree/expression/function/datetime/LocalDateTimeExpressionFunctionNowTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression.function.datetime;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.tree.expression.function.ExpressionFunctionContext;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public final class LocalDateTimeExpressionFunctionNowTest extends LocalDateTimeExpressionFunctionTestCase<LocalDateTimeExpressionFunctionNow<ExpressionFunctionContext>> {
+
+    @Test
+    public void testApply() {
+        final LocalDateTime date = this.apply2();
+        assertTrue(
+                date.getYear() >= 2021,
+                () -> date + " year >= 2021"
+        );
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(this.createBiFunction(), "now");
+    }
+
+    @Override
+    public LocalDateTimeExpressionFunctionNow<ExpressionFunctionContext> createBiFunction() {
+        return LocalDateTimeExpressionFunctionNow.instance();
+    }
+
+    @Override
+    public Class<LocalDateTimeExpressionFunctionNow<ExpressionFunctionContext>> type() {
+        return Cast.to(LocalDateTimeExpressionFunctionNow.class);
+    }
+}
