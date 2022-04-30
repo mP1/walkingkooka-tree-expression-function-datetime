62abc126b14ea3207c21963980e11c532724356d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-04-30 22:30:31 +1000

    https://github.com/mP1/walkingkooka-tree/pull/484 ExpressionFunction.kinds()
    - https://github.com/mP1/walkingkooka-tree/pull/484
    - ExpressionFunction.kinds()

--- src/main/java/walkingkooka/tree/expression/function/datetime/LocalDateExpressionFunction.java
+++ src/main/java/walkingkooka/tree/expression/function/datetime/LocalDateExpressionFunction.java
@@ -21,8 +21,11 @@ import walkingkooka.tree.expression.ExpressionPurityContext;
 import walkingkooka.tree.expression.FunctionExpressionName;
 import walkingkooka.tree.expression.function.ExpressionFunction;
 import walkingkooka.tree.expression.function.ExpressionFunctionContext;
+import walkingkooka.tree.expression.function.ExpressionFunctionKind;
 
 import java.time.LocalDate;
+import java.util.EnumSet;
+import java.util.Set;
 
 abstract class LocalDateExpressionFunction<C extends ExpressionFunctionContext> implements ExpressionFunction<LocalDate, C> {
 
@@ -44,14 +47,14 @@ abstract class LocalDateExpressionFunction<C extends ExpressionFunctionContext> 
     }
 
     @Override
-    public final boolean requiresEvaluatedParameters() {
-        return true;
+    public final Set<ExpressionFunctionKind> kinds() {
+        return KINDS;
     }
 
-    @Override
-    public final boolean resolveReferences() {
-        return true;
-    }
+    private final Set<ExpressionFunctionKind> KINDS = EnumSet.of(
+            ExpressionFunctionKind.REQUIRES_EVALUATED_PARAMETERS,
+            ExpressionFunctionKind.RESOLVE_REFERENCES
+    );
 
     @Override
     public final boolean isPure(ExpressionPurityContext expressionPurityContext) {
