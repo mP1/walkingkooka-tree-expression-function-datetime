e46c151338d5f87e0812e96bca1117d63057adb6
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-05-04 20:12:15 +1000

    https://github.com/mP1/walkingkooka-tree/pull/504 ExpressionFunctionKind.EVALUATE_PARAMETERS was REQUIRES-EVALUATED_PARAMETERS
    - https://github.com/mP1/walkingkooka-tree/pull/504
    - ExpressionFunctionKind.EVALUATE_PARAMETERS was REQUIRES-EVALUATED_PARAMETERS

--- src/main/java/walkingkooka/tree/expression/function/datetime/LocalTimeExpressionFunction.java
+++ src/main/java/walkingkooka/tree/expression/function/datetime/LocalTimeExpressionFunction.java
@@ -52,7 +52,7 @@ abstract class LocalTimeExpressionFunction<C extends ExpressionFunctionContext> 
     }
 
     private final Set<ExpressionFunctionKind> KINDS = EnumSet.of(
-            ExpressionFunctionKind.REQUIRES_EVALUATED_PARAMETERS,
+            ExpressionFunctionKind.EVALUATE_PARAMETERS,
             ExpressionFunctionKind.RESOLVE_REFERENCES
     );
 
